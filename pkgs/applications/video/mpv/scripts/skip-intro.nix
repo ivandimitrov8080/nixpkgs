70c0d669adc78029fe6b9a72a4b08d73158ff5b8
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-02-17 23:03:27 +0100

    skip-intro script

---
+++ pkgs/applications/video/mpv/scripts/skip-intro.nix
@@ -0,0 +1,25 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildLua,
+  mpv-unwrapped,
+}:
+
+buildLua {
+  pname = "skip-intro";
+  name = "skip-intro";
+
+  src = fetchFromGitHub {
+    owner = "rui-ddc";
+    repo = "skip-intro";
+    rev = "3a7d2e95d5adfa761e1a25097022403f3b8a0cba";
+    hash = "sha256-1rxwPiYIj4oxy8piRpiJeWU4b8vCnLCOvxt7uoru5xc=";
+  };
+
+  meta = {
+    description = "This is a script for skipping episode intros (on mpv media player).";
+    homepage = "https://github.com/rui-ddc/skip-intro";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ ivandimitrov8080 ];
+  };
+}
